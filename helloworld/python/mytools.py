e88dd1ce0fe630778297ac6dd9d7e41c62394858
symyself <symyself@163.com>
2015-11-13 11:31:45 +0800

    change mytools.py

--- helloworld/python/mytools.py
+++ helloworld/python/mytools.py
@@ -4,5 +4,12 @@ def name():
     print 'songy'
     print 'symyself@163.com'
 
+def exec_str(info,cmd):
+    print "%30s:%10s = %s" %(info,cmd,str(eval(cmd)))
+def convert_func():
+    print '常用的内置函数'
+    exec_str('int[0,256) to char','chr(48)')
+    exec_str('char to int','ord("0")')
+    
 if __name__ == "__main__":
-    name()
+    convert_func()
